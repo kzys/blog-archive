b82d628372d6430759b022aecd7f941cb2ee3c5a
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2011-05-01 09:00:38 +0900

    Add new index.html.

--- make-index.rb
+++ make-index.rb
@@ -28,6 +28,56 @@ def create_index(year)
   File.open("#{year}/index.html", 'w').print(template.result(binding))
 end
 
+def create_index_2010(year, files, pattern)
+  by_month = Hash.new do
+    []
+  end
+
+  files.each do |path|
+    content = path.read
+    title = content.scan(%r{<title>(.*)</title>})[0][0]
+    year, month, day = *content.scan(pattern)[0]
+    title = title.gsub(/^blog\.8-p\.info: /, '').gsub(/ - blog\.8-p\.info$/, '')
+    by_month[month.to_i] += [ { month: month,
+                                title: title,
+                                path: path,
+                                day: day } ]
+  end
+
+  months = by_month.keys.sort.map do |m|
+    [m, by_month[m].sort do |a, b| a[:day] <=> b[:day] end]
+  end
+
+  template = ERB.new(File.read('index.html.erb'))
+  File.open("#{year}/index.html", 'w').print(template.result(binding))
+end
+
 create_index(2005)
 create_index(2006)
 create_index(2007)
+
+create_index_2010(2008,
+                  Dir.glob("2008/*").map do |s|
+                    Pathname(s)
+                  end.delete_if do |path|
+                    not path.basename.to_s =~ /^\d+$/
+                  end,
+                  %r{(\d{4})-(\d{2})-(\d{2})T})
+
+create_index_2010(2009,
+                  Dir.glob("2009/*/*").map do |s|
+                    Pathname(s)
+                  end.delete_if do |path|
+                    not path.to_s =~ %r{^\d{4}/\d{2}/}
+                  end,
+                  %r{(\d{4})-(\d{2})-(\d{2})})
+
+create_index_2010(2010,
+                  Dir.glob("2010/*").sort do |a, b|
+                    a.to_i <=> b.to_i
+                  end.map do |s|
+                    Pathname(s)
+                  end.delete_if do |path|
+                    not path.basename.to_s =~ /^\d+-?/
+                  end,
+                  %r{Posted at (\d{4})/(\d{2})/(\d{2})})
