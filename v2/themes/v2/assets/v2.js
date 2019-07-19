8c2df80772816ae50c9b205b8854ad03dafcd5f5
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2019-07-19 07:46:31 -0700

    Add h2 on each year

--- v2/themes/v2/assets/v2.js
+++ v2/themes/v2/assets/v2.js
@@ -1,14 +1,26 @@
 let timeParse = d3.timeParse("%Y-%m-%dT%H:%M:%S%Z");
 let timeFormat = d3.timeFormat("%Y-%m-%d");
 
-function addPosts(selection, items) {
-    let li = selection
-        .selectAll('li')
+function addPosts(selection, items) {    
+    let div = selection
+        .selectAll('div')
         .data(items)
-        .join('li');
+        .join('div')
+
+    div.append('h2')
+        .text(x => x.year)
+
+    let li = div
+        .selectAll('li')
+        .data(x => {
+            return x.items;
+        })
+        .join('li')
 
     li.append('span')
-        .text(x => timeFormat(timeParse(x.published)) + ' ');
+        .attr('class', 'date')
+        .text(x => timeFormat(x.date) + ' ');
+    
     li.append('a')
         .text(x => x.title)
         .attr('href', x => x.url);
@@ -24,9 +36,23 @@ async function main() {
     items.sort((a, b) => {
         return a.published.localeCompare(b.published) * -1;
     })
+    items = items.map(x => {
+        return { ...x, date: timeParse(x.published) };
+    })
 
-    console.log(items);
-    addPosts(d3.select('#root'), items);
+    let years = {}
+    items.forEach(x => {
+        let k = x.date.getFullYear()
+        years[k] = years[k] || []
+        years[k].push(x)
+    })
+    let yearsList = []
+    for (k in years) [
+        yearsList.push({ year: k, items: years[k] })
+    ]
+    yearsList.reverse()
+
+    addPosts(d3.select('#root'), yearsList);
 }
 
 main();
