b45040e90d27dafb6e16e1feb1f09d574526a6ba
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2019-12-16 22:18:22 -0800

    Japanese is in red, English is in blue

--- v2/themes/v2/assets/v2.js
+++ v2/themes/v2/assets/v2.js
@@ -31,16 +31,21 @@ function enterDiv(div) {
             x.items.forEach(i => {
                 let key = ymd(i.date);
                 if (dateToPostCount[key]) {
-                    dateToPostCount[key]++
+                    dateToPostCount[key][i.language]++
                 } else {
-                    dateToPostCount[key] = 1
+                    dateToPostCount[key] = {}
+                    dateToPostCount[key][i.language] = 1
                 }
             })
             let begin = new Date(x.year, 1, 1)
             let xs = d3.timeDay.range(d3.timeYear.floor(begin), d3.timeYear.ceil(begin));
             return xs.map(d => {
                 let key = ymd(d);
-                return { date: d, count: dateToPostCount[key] || 0 }
+                return {
+                    date: d,
+                    count: dateToPostCount[key] || {},
+                    empty: ! dateToPostCount[key],
+                };
             })
         })
         .join('circle')
@@ -54,7 +59,16 @@ function enterDiv(div) {
             return monthToY(d.getMonth()) + weekToY(week);
         })
         .attr('r', function (x) {
-            return 2 + x.count;
+            return 1 + (x.empty ? 0 : 2);
+        })
+        .attr('fill', function (x) {
+            if (x.count.ja) {
+                return '#f00'
+            } else if (x.count.en) {
+                return '#00f'
+            } else {
+                return '#000'
+            }
         });
 
     let ul = div.select('ul')
@@ -68,11 +82,11 @@ function enterDiv(div) {
             let li = enter.append('li')
                 .attr('class', d => 'lang-' + d.language)
 
-            
+
             li.append('span')
                 .attr('class', 'date')
                 .text(x => timeFormat(x.date) + ' ');
-        
+
             li.append('a')
                 .text(x => x.title)
                 .attr('href', x => x.url);
@@ -121,7 +135,7 @@ async function render(items) {
 function parseAndSort(items) {
     let parsed = items.map(x => {
         let date = timeParse(x.published) || timeParse2(x.published) || timeParse3(x.published) || timeParse4(x.published);
-        if (date == null) {            
+        if (date == null) {
             console.log("Failed to parse " + x.published)
         }
         return { ...x, date: date };
@@ -172,6 +186,6 @@ async function loadYear(year) {
         render(filtered)
     } else {
         let xs = await d3.json(`/json/${year}.json`);
-        render(parseAndSort(xs.items));    
+        render(parseAndSort(xs.items));
     }
 }
